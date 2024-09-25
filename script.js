f8a98f25333f9d0aad47edfa138f42bee6ab9e2b
Ayren Killman <ayrenkillman@gmail.com>
2024-09-25 14:02:31 -0500

    Added more clickers, removed stuff, etc.

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const clickCounter = document.getElementById('click-counter');
 let totalMoney = clickCounter.innerText = 0;
 let Multiplier = 1;
+let lastClickTime = 0
 // Update the click counter
 function updateClickCounter() {
+    const currentTime = Date.now();
+    if (currentTime - lastClickTime >= 300) {
+        totalMoney = totalMoney + Multiplier;
+        clickCounter.innerText = totalMoney;
+        lastClickTime = currentTime;
+    }
+}
+
+function updateClickCounterAuto() {
     totalMoney = totalMoney + Multiplier;
     clickCounter.innerText = totalMoney;
+    lastClickTime = currentTime;
 }
 function CostofItem(Price) {
     if (totalMoney >= Price) {
@@ -14,20 +25,32 @@ function CostofItem(Price) {
     }
 }
 
-function DoubleMultiplier() {
-    if (CostofItem(50) == false) {
-        return false;
-    } else {
-        Multiplier = Multiplier * 2;
-        clickCounter.innerText = totalMoney;
-    }
-}
+
 
 function autoClicker() {
     if (CostofItem(10) == false) {
         return false;
     } else {
-        setInterval(updateClickCounter, 10000);
+
+        setInterval(updateClickCounterAuto, 10000);
+        clickCounter.innerText = totalMoney;
+    }
+}
+
+function FasterAutoClicker() {
+    if (CostofItem(50) == false) {
+        return false;
+    } else {
+        setInterval(updateClickCounterAuto, 6000);
+        clickCounter.innerText = totalMoney;
+    }
+}
+
+function EfficientAutoClicker() {
+    if (CostofItem(100) == false) {
+        return false;
+    } else {
+        setInterval(updateClickCounterAuto, 1000);
         clickCounter.innerText = totalMoney;
     }
 }
